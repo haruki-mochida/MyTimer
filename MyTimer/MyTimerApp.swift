12cd520054527950c822cfcde4baff41dda04af6
mochidaharuki <mochidaharuki@plise.co.jp>
2023-04-25 12:13:40 +0900

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by 持田晴生 on 2023/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
